ffdcd4bcd79ba78d003fc88711ee68d57d649d3a
platypuppy <61122248+platypuppy@users.noreply.github.com>
2020-09-03 23:47:22 -0700

    Added GTest and 2 Unit Tests

--- _game/subsystems/ObjectSerilazation/UnitTestDriver-1.cpp
+++ _game/subsystems/ObjectSerilazation/UnitTestDriver-1.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-#include "rapidjson/document.h"
-#include "rapidjson/istreamwrapper.h"
 #include <string>
 #include <fstream>
-#include "rapidjson/error/en.h"
-#include "rapidjson/error/error.h"
 #include <vector>
+
+#include <gtest/gtest.h>
 
 //#include "DeSerializable.cpp"
 //#include "DSObject.h"
 
 #include "ObjectDeSerilazation.h"
 
+/*friend ostream& operator << (ostream& os, const DSObject& dso) {
+  os << dso.number << dso.flot;
+  return os;
+}
+
 static const char* BoolConvert[] = {
   "false",
   "true"
 };
+
 
 std::string operator * (const std::string& string, int times) {
   std::string out;
@@ -23,17 +27,29 @@ std::string operator * (const std::string& string, int times) {
     out += string;
 
   return out;
+}*/
+
+
+TEST(TestingTest, test) {
+  ASSERT_EQ(-1, -1);
 }
 
-int main (void) {
-  std::vector<DeSerializable*>* objects = ObjectSerializer::DeSerialize("_in/test.json");
+TEST(TestingTest, otherTest) {
+  ASSERT_EQ(1, -1);
+}
+
+
+int main (int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
   
-    
+  /*  std::vector<DeSerializable*>* objects = ObjectSerializer::DeSerialize("_in/test.json");
+  
   std::cout << "delete?" << std::endl;
   for (long long int i = objects->size()-1; i >= 0; i--) {
     std::cout << "would delete: " << (*objects)[i] << std::endl;
     delete (*objects)[i];
   }
-  delete objects;
+  delete objects;*/
 }
 
